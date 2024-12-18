2d322a09647b4da0c4b1974a303ff9caa204bf91
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2024-12-18 17:55:01 +0000

    Adds new SQL jobs and models for Airflow scheduled Snowflake project - Introduces jobs: SQL_Composer_AirflowScheduled_Snowflake, SQL_MWAA_AirflowScheduled_Snowflake, and SQL_PMAir_AirflowScheduled_Snowflake - Adds new SQL model: testModelSchdRun_Automation_Snowflake_Project_AirflowScheduled - Includes a new SQL seed: testSeedSchdRun_Automation_Snowflake_Project_AirflowScheduled - Implements a new SQL macro: generate_schema_name

---
+++ releasesql/1734544062319/sqlProject/jobs/SQL_PMAir_AirflowScheduled_Snowflake/code/k54cfbpadqljk7eveeuqda_/tasks/Model_0.py
@@ -0,0 +1,41 @@
+from k54cfbpadqljk7eveeuqda_.utils import *
+
+def Model_0():
+    from airflow.operators.python import PythonOperator
+    from datetime import timedelta
+    import os
+    import zipfile
+    import tempfile
+
+    return PythonOperator(
+        task_id = "Model_0",
+        python_callable = invoke_dbt_runner,
+        op_kwargs = {
+          "is_adhoc_run_from_same_project": False,
+          "is_prophecy_managed": True,
+          "run_deps": False,
+          "run_seeds": True,
+          "run_parents": False,
+          "run_children": False,
+          "run_tests": True,
+          "run_mode": "project",
+          "entity_kind": None,
+          "entity_name": None,
+          "project_id": "6963",
+          "git_entity": "branch",
+          "git_entity_value": "main",
+          "git_ssh_url": "https://github.com/abhisheksr8/scheduledJobs_Snowflake.git",
+          "git_sub_path": "releasesql/1734544062319/sqlProject",
+          "select": "",
+          "threads": "",
+          "exclude": "",
+          "run_props": " --profile run_profile",
+          "envs": {
+            "DBT_DATABRICKS_INVOCATION_ENV": "prophecy", 
+            "DBT_SEND_ANONYMOUS_USAGE_STATS": "false", 
+            "DBT_FULL_REFRESH": "true"
+          }, 
+          "git_token_secret": "fHmjOFKiDk_Tk97DTG8qsA_", 
+          "dbt_profile_secret": "XPyjYLy4mZ34yu7m__eyz"
+        },
+    )
